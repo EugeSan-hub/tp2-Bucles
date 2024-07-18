fdf09561aae75d0cf7eca29b9d62817a797c5e02
Eugenia Sansierra <eugesans@gmail.com>
2024-07-18 11:08:49 -0300

    se puso script index ejercio 17 y se activo comentario de ejercico para ver

---
+++ js/ejercicio17.js
@@ -0,0 +1,7 @@
+// Realiza un script que muestre la posición de la primera vocal de un texto introducido por teclado.
+// Ejemplo:
+// Input: Hola mundo
+// Output: la vocal ‘o’ está en la posición 1
+
+let texto = prompt("Ingrese una cadena de texto:").toUpperCase();
+// NO SE COMO RESOLVERLO. 
