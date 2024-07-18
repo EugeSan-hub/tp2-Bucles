e38fdd9a3820010b086bec86132f18fa6b958ead
Eugenia Sansierra <eugesans@gmail.com>
2024-07-18 11:01:29 -0300

    se realizo ejercicio16

---
+++ ejercicio16.js
@@ -0,0 +1,8 @@
+// // Realiza un script que pida una cadena de texto y la devuelva al revés
+// . Es decir, si tecleo “hola que tal” deberá mostrar “lat euq aloh”.
+
+let texto = prompt("Ingrese un texto aqui");
+let textoInvertido = texto.split("").reverse().join("");
+alert("El texto invertido es: " + textoInvertido);
+
+// utilice chatgpt para saber que propiedad usar para invertir el texto.
